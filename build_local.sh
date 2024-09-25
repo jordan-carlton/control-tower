32d1beb9338237aaa5475efe02391060fa1d6610
Jordan Carlton <jordan.carlton@softwire.com>
2024-09-25 10:53:42 +0100

    Update build local script for amd64

--- build_local.sh
+++ build_local.sh
@@ -2,16 +2,16 @@
 
 set -eu
 
-version=dev
+version=0.32.0
 cp ../control-tower-ops/manifest.yml opsassets/assets/
-cp -R ../control-tower-ops/ops opsassets/assets/ 
+cp -R ../control-tower-ops/ops opsassets/assets/
 cp ../control-tower-ops/createenv-dependencies-and-cli-versions-aws.json opsassets/assets/
 cp ../control-tower-ops/createenv-dependencies-and-cli-versions-gcp.json opsassets/assets/
-GO111MODULE=on go build -mod=vendor -ldflags "
+GO111MODULE=on env GOOS=linux GOARCH=amd64 go build -mod=vendor -ldflags "
   -X github.com/EngineerBetter/control-tower/fly.ControlTowerVersion=$version
   -X main.ControlTowerVersion=$version
-" -o control-tower
+" -o control-tower-linux-amd64
 
-chmod +x control-tower
+chmod +x control-tower-linux-amd64
 
 echo "$PWD/control-tower"
